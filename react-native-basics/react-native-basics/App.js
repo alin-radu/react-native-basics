9d171b21d39bf38608b4b8f01a0497d6e83c3269
Alin RADU <alin.radu.cristian@gmail.com>
2023-08-28 12:33:54 +0300

    add react-native-basics

---
+++ react-native-basics/react-native-basics/App.js
@@ -0,0 +1,42 @@
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Course Goal!"></TextInput>
+        <Button title="Add Goal" />
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    height: '100%',
+    paddingTop: 50,
+    paddingHorizontal: 60,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  textInput: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+});
---
+++ react-native-basics/react-native-basics/INFO.md
@@ -0,0 +1,17 @@
+## INFO
+
+### I. TUTORIAL
+
+#### 1. React Native - The Practical Guide
+
+- title: React Native - The Practical Guide
+- author: Maximilian Schwarzmüller
+- link: https://www.udemy.com/course/react-native-the-practical-guide/
+
+### II. GITHUB
+
+- git@github.com:alin-radu/react-native-basics.git
+
+### III. APP
+
+- ...
