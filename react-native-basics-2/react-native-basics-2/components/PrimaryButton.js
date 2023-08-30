7bed1221a3a56378339c074b4aec6199c5a0d12a
Alin RADU <alin.radu.cristian@gmail.com>
2023-08-30 13:40:39 +0300

    react-native-basics-2, add StartGameScreen

--- react-native-basics-2/react-native-basics-2/components/PrimaryButton.js
+++ react-native-basics-2/react-native-basics-2/components/PrimaryButton.js
@@ -1,10 +1,40 @@
-import { Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 export const PrimaryButton = (props) => {
-  const { children } = props;
+  const { children, onPress } = props;
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.outerContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [styles.innerContainer, styles.pressed] : styles.innerContainer
+        }
+        onPress={onPress}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.text}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  outerContainer: {
+    borderRadius: 28,
+    margin: 8,
+    overflow: 'hidden',
+  },
+  innerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 3,
+  },
+  text: {
+    color: '#fff',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- react-native-basics-2/react-native-basics-2/screens/StartGameScreen.js
+++ react-native-basics-2/react-native-basics-2/screens/StartGameScreen.js
@@ -1,27 +1,107 @@
-import { StyleSheet, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, StyleSheet, TextInput, View } from 'react-native';
 import { PrimaryButton } from '../components/PrimaryButton';
 
+const showIsInvalidNumberAlert = (onConfirmCallback) => {
+  return Alert.alert(
+    'Invalid number!',
+    'The entered value has to be a number between 1 and 99.',
+    [
+      {
+        text: 'Okay',
+        style: 'destructive',
+        onPress: onConfirmCallback,
+      },
+    ]
+  );
+};
+
 export const StartGameScreen = () => {
+  const [enteredNumber, setEnteredNumber] = useState('');
+
+  const numberInputHandler = (enteredText) => {
+    setEnteredNumber(enteredText);
+  };
+
+  const resetInputHandler = () => {
+    setEnteredNumber('');
+  };
+
+  const confirmInputHandler = () => {
+    const selectedNumber = parseInt(enteredNumber);
+    const isInvalidValidNumber =
+      isNaN(selectedNumber) || selectedNumber <= 0 || selectedNumber > 99;
+
+    if (isInvalidValidNumber) {
+      showIsInvalidNumberAlert(resetInputHandler);
+      return;
+    }
+
+    console.log('Valid Number');
+  };
+
   return (
-    <View style={styles.mainContainer}>
-      <View>
-        <TextInput />
-      </View>
-      <View>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+    <View style={styles.mainOuterContainer}>
+      <View style={styles.mainInnerContainer}>
+        <TextInput
+          style={styles.input}
+          maxLength={2}
+          keyboardType="number-pad"
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={enteredNumber}
+          onChangeText={numberInputHandler}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.button}>
+            <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+          </View>
+          <View style={styles.button}>
+            <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+          </View>
+        </View>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  mainContainer: {
+  mainOuterContainer: {
     marginTop: 100,
-    marginHorizontal: 32,
-    padding: 16,
+    marginHorizontal: 24,
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     elevation: 20,
-    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  mainInnerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    borderRadius: 8,
+  },
+  input: {
+    width: 75,
+    height: 50,
+    marginVertical: 8,
+    textAlign: 'center',
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#ddb52f',
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginTop: 8,
+  },
+  button: {
+    flex: 1,
   },
 });
